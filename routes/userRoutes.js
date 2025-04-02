35e47bff09c8df7ba1b5694e6ccda521db6801f2
Lavesh Vyas <laveshvyas20@gmail.com>
2025-04-02 22:24:13 +0530

    added googleauth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,12 +9,13 @@
 // module.exports = router;
 
 const express = require('express');
-const { userSignup, userLogin, forgotPassword, resetPassword } = require('../controllers/userAuthController');
+const { userSignup, userLogin, forgotPassword, resetPassword , googleAuth } = require('../controllers/userAuthController');
 
 const router = express.Router();
 
 router.post('/register', userSignup);
 router.post('/signin', userLogin);
+router.post("/google-auth", googleAuth);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
 
